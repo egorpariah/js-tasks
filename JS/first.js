608c7c3a0386ffac17c5425f3e3fa2d397d68f27
Egor Golikov <elai.asti@gmail.com>
2022-07-04 02:23:10 +0700

    Сделал первую задачу через ES6

--- JS/first.js
+++ JS/first.js
@@ -2,16 +2,8 @@ const vowels = ['а', 'у', 'о', 'ы', 'э', 'я', 'ю', 'ё', 'и', 'е'];
 const input = 'Все любят своеволие, но страшатся свободы.';
 
 const getVowels = string => {
-  string = string.toLowerCase();
-  let output = '';
-
-  for (let i = 0; i < string.length; i++) {
-      if (vowels.includes(string[i])) {
-        output += string[i];
-      }
-    }
-
-  return output;
-}
-
-console.log('Гласные из фразы: ', getVowels(input))
+  return string.toLowerCase().split('').filter(e => 
+    vowels.includes(e) ? e : '').join();
+  }
+  
+console.log('Гласные из фразы: ', getVowels(input));
